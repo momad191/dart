1688ffb19937f933e7dc337125a685df3b39c46e
Mohammed Emad <moemad191@gmail.com>
2025-07-19 23:24:42 +0300

    first commit

---
+++ main.dart
@@ -0,0 +1,11 @@
+void main() {
+  // code
+  String first_name = "mohammed";
+  String last_name = "emad";
+  int age = 12;
+  double w = 2.5;
+  List l = [];
+  bool r = true;
+
+  print("$first_name $last_name is $age has $w");
+}
