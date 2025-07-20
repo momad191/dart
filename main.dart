419700686eff7aa07ea022c7098b809f2cee2e68
Mohammed Emad <moemad191@gmail.com>
2025-07-20 22:36:24 +0300

    operators

--- main.dart
+++ main.dart
@@ -1,11 +1,67 @@
 void main() {
   // code
-  String first_name = "mohammed";
-  String last_name = "emad";
-  int age = 12;
-  double w = 2.5;
-  List l = [];
-  bool r = true;
+  // String first_name = "mohammed";
+  // String last_name = "emad";
+  // int age = 12;
+  // double w = 2.5;
+  // List l = [];
+  // bool r = true;
 
-  print("$first_name $last_name is $age has $w");
+  // print("$first_name $last_name is $age has $w");
+
+  //null problem
+  // String? name;
+  // print(name);
+
+  //null safty
+  // String? name2;
+  // print(name2!.length);
+
+  // late use to declare variable for future use
+  // late String name3;
+  // name3 = "mm";
+  // print(name3);
+
+  // int a = 10;
+  // int b = 4;
+  // int c = a + b;
+  // int c = a - b;
+  // int c = a * b;
+  // int c = a % b;
+  // double c = a / b;
+  // int c = a ~/ b; // 2.5 = 2
+  // int c = 10;
+  // c++;   increment
+  // c--;   decrement
+  // c = c + 1;
+
+  // int a = 20;
+  // int b = 20;
+  // print(a != b);
+  // print(a > b);
+  // print(a >= b);
+
+  //testing operators
+
+  // String a = "A";
+  // print(a is! int);
+
+  // String a = "A";
+  // print(a is int);
+
+  // int a = 10;
+  // a = 20;
+  // print(a);
+
+  // int a = 1;
+  // a += 20;
+  // print(a);
+
+  // double a = 100;
+  // a /= 20;
+  // print(a);
+
+  // int a = 20;
+  // int b = 30;
+  // print(!(a > 10 || b > 10));
 }
